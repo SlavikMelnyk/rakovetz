46cc3ffa5c356ce40c5fd196305a71e69231ecfc
Yaroslav Melnyk <yaroslav.melnyk@dataart.com>
2019-09-05 18:44:32 +0300

    add image optimization

--- src/home/Carousel.js
+++ src/home/Carousel.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './home.scss';
+import Img from 'react-image'
 import { Carousel } from 'react-bootstrap';
-import img1 from './carousel-imgs/jpg-01.jpg';
-import img2 from './carousel-imgs/jpg-02.jpg';
-import img3 from './carousel-imgs/jpg-03.jpg';
+import img1 from './carousel-imgs/jpg-01-min.jpg';
+import img2 from './carousel-imgs/jpg-02-min.jpg';
+import img3 from './carousel-imgs/jpg-03-min.jpg';
 
 export default () => {
   return(
     <Carousel>
       <Carousel.Item>
-        <img className="d-block w-100" src={img1} alt="slide-1" />
+        <Img className="d-block w-100" src={img1} alt="slide-1" />
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={img2} alt="slide-2" />
+        <Img className="d-block w-100" src={img2} alt="slide-2" />
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={img3} alt="slide-3" />
+        <Img className="d-block w-100" src={img3} alt="slide-3" />
       </Carousel.Item>
     </Carousel>
   );
--- src/home/Homepage.js
+++ src/home/Homepage.js
@@ -3,15 +3,17 @@ import Carousel from './Carousel';
 import ReactPlayer from 'react-player';
 import promo from './promo.mp4';
 import GoogleMap from './GoogleMap';
-
+import ObserverWrapper from '@emarketeross/simple-react-intersection-observer';
 
 export default () => {
   return(
     <React.Fragment>
       <Carousel/>
       <div className='content-wrapper'>
-        <ReactPlayer url={promo} playing={true} controls volume={0} width='50%' height='auto' />
+      <ReactPlayer url={promo} playing={true} controls volume={0} width='50%' height='auto' />
+      <ObserverWrapper>
         <GoogleMap />
+      </ObserverWrapper>
       </div>
     </React.Fragment>
   );
