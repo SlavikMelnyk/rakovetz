8e4a1b66929538d5828e7f4cb5f7a8ee2726577d
Yaroslav Melnyk <yaroslav.melnyk@dataart.com>
2019-09-19 17:55:37 +0300

    change gallery page

--- src/gallery/Gallery.js
+++ src/gallery/Gallery.js
@@ -95,95 +95,97 @@ export default (props) => {
   }, []);
   
   return (
-    <div className='gallery-container'>
-      <h1>Галерея</h1>
-      <div className='img-gallery-container'>
-        <div className='images'>
-          <a name="houses" /> {/* eslint-disable-line */}
-          <h2>Котеджі:</h2>
-          <img data-lazy={himg1} alt='himg1' />
-          <img data-lazy={himg2} alt='himg2' />
-          <img data-lazy={himg3} alt='himg3' />
-          <img data-lazy={himg4} alt='himg4' />
-          <img data-lazy={himg5} alt='himg5' />
-          <img data-lazy={himg6} alt='himg6' />
-          <img data-lazy={himg7} alt='himg7' />
-          <img data-lazy={himg8} alt='himg8' />
-          <img data-lazy={himg9} alt='himg9' />
-          <img data-lazy={himg10} alt='himg10' />
-          <img data-lazy={himg11} alt='himg11' />
-          <img data-lazy={himg12} alt='himg12' />
-          <img data-lazy={himg13} alt='himg13' />
-          <img data-lazy={himg14} alt='himg14' />
-          <img data-lazy={himg15} alt='himg15' />
-          <img data-lazy={himg16} alt='himg16' />
-          <img data-lazy={himg17} alt='himg17' />
-          <img data-lazy={himg18} alt='himg18' />
-        </div>
-        <div className='images'>
-          <a name="sauna" /> {/* eslint-disable-line */}
-          <h2>Сауна/Чан:</h2>
-          <img data-lazy={simg1} alt='simg1' />
-          <img data-lazy={simg2} alt='simg2' />
-          <img data-lazy={simg3} alt='simg3' />
-          <img data-lazy={simg4} alt='simg4' />
-          <img data-lazy={simg5} alt='simg5' />
-        </div>
-        <div className='images'>
-          <a name="lake" /> {/* eslint-disable-line */}
-          <h2>Альтанка на озері:</h2>
-          <img data-lazy={limg1} alt='limg1' />
-          <img data-lazy={limg2} alt='limg2' />
-          <img data-lazy={limg3} alt='limg3' />
-          <img data-lazy={limg4} alt='limg4' />
-          <img data-lazy={limg5} alt='limg5' />
-          <img data-lazy={limg6} alt='limg6' />
-        </div>
-        <div className='images'>
-          <a name="location" /> {/* eslint-disable-line */}
-          <h2>Локація:</h2>
-          <img data-lazy={lcimg1} alt='lcimg1' />
-          <img data-lazy={lcimg2} alt='lcimg2' />
-          <img data-lazy={lcimg3} alt='lcimg3' />
-          <img data-lazy={lcimg4} alt='lcimg4' />
-          <img data-lazy={lcimg5} alt='lcimg5' />
-          <img data-lazy={lcimg6} alt='lcimg6' />
-          <img data-lazy={lcimg9} alt='lcimg9' />
-          <img data-lazy={lcimg10} alt='lcimg10' />
-        </div>
-        <div className='images'>
-          <a name="moments" /> {/* eslint-disable-line */}
-          <h2>Моменти:</h2>
-          <img data-lazy={mimg1} alt='mimg1' />
-          <img data-lazy={mimg2} alt='mimg2' />
-          <img data-lazy={mimg3} alt='mimg3' />
-          <img data-lazy={mimg4} alt='mimg4' />
-          <img data-lazy={mimg5} alt='mimg5' />
-          <img data-lazy={mimg6} alt='mimg6' />
-          <img data-lazy={mimg7} alt='mimg7' />
-          <img data-lazy={mimg8} alt='mimg8' />
-          <img data-lazy={mimg9} alt='mimg9' />
-          <img data-lazy={mimg10} alt='mimg10' />
-          <img data-lazy={mimg11} alt='mimg11' />
-          <img data-lazy={mimg12} alt='mimg12' />
-          <img data-lazy={mimg13} alt='mimg13' />
-          <img data-lazy={mimg14} alt='mimg14' />
-          <img data-lazy={mimg15} alt='mimg15' />
-          <img data-lazy={mimg16} alt='mimg16' />
-          <img data-lazy={mimg17} alt='mimg17' />
-          <img data-lazy={mimg18} alt='mimg18' />
-        </div>
-        <div className='images'>
-          <a name="winter" /> {/* eslint-disable-line */}
-          <h2>Зима:</h2>
-          <img data-lazy={wimg1} alt='wimg1' />
-          <img data-lazy={wimg2} alt='wimg2' />
-          <img data-lazy={wimg3} alt='wimg3' />
-          <img data-lazy={wimg4} alt='wimg4' />
-          <img data-lazy={wimg5} alt='wimg5' />
-          <img data-lazy={wimg6} alt='wimg6' />
-          <img data-lazy={wimg7} alt='wimg7' />
-          <img data-lazy={wimg8} alt='wimg8' />
+    <React.Fragment>
+      <div className='gallery-container'>
+        <h1>Галерея</h1>
+        <div className='img-gallery-container'>
+          <div className='images'>
+            <a name="houses" /> {/* eslint-disable-line */}
+            <h2>Котеджі:</h2>
+            <img data-lazy={himg1} alt='himg1' />
+            <img data-lazy={himg2} alt='himg2' />
+            <img data-lazy={himg3} alt='himg3' />
+            <img data-lazy={himg4} alt='himg4' />
+            <img data-lazy={himg5} alt='himg5' />
+            <img data-lazy={himg6} alt='himg6' />
+            <img data-lazy={himg7} alt='himg7' />
+            <img data-lazy={himg8} alt='himg8' />
+            <img data-lazy={himg9} alt='himg9' />
+            <img data-lazy={himg10} alt='himg10' />
+            <img data-lazy={himg11} alt='himg11' />
+            <img data-lazy={himg12} alt='himg12' />
+            <img data-lazy={himg13} alt='himg13' />
+            <img data-lazy={himg14} alt='himg14' />
+            <img data-lazy={himg15} alt='himg15' />
+            <img data-lazy={himg16} alt='himg16' />
+            <img data-lazy={himg17} alt='himg17' />
+            <img data-lazy={himg18} alt='himg18' />
+          </div>
+          <div className='images'>
+            <a name="sauna" /> {/* eslint-disable-line */}
+            <h2>Сауна/Чан:</h2>
+            <img data-lazy={simg1} alt='simg1' />
+            <img data-lazy={simg2} alt='simg2' />
+            <img data-lazy={simg3} alt='simg3' />
+            <img data-lazy={simg4} alt='simg4' />
+            <img data-lazy={simg5} alt='simg5' />
+          </div>
+          <div className='images'>
+            <a name="lake" /> {/* eslint-disable-line */}
+            <h2>Альтанка на озері:</h2>
+            <img data-lazy={limg1} alt='limg1' />
+            <img data-lazy={limg2} alt='limg2' />
+            <img data-lazy={limg3} alt='limg3' />
+            <img data-lazy={limg4} alt='limg4' />
+            <img data-lazy={limg5} alt='limg5' />
+            <img data-lazy={limg6} alt='limg6' />
+          </div>
+          <div className='images'>
+            <a name="location" /> {/* eslint-disable-line */}
+            <h2>Локація:</h2>
+            <img data-lazy={lcimg1} alt='lcimg1' />
+            <img data-lazy={lcimg2} alt='lcimg2' />
+            <img data-lazy={lcimg3} alt='lcimg3' />
+            <img data-lazy={lcimg4} alt='lcimg4' />
+            <img data-lazy={lcimg5} alt='lcimg5' />
+            <img data-lazy={lcimg6} alt='lcimg6' />
+            <img data-lazy={lcimg9} alt='lcimg9' />
+            <img data-lazy={lcimg10} alt='lcimg10' />
+          </div>
+          <div className='images'>
+            <a name="moments" /> {/* eslint-disable-line */}
+            <h2>Моменти:</h2>
+            <img data-lazy={mimg1} alt='mimg1' />
+            <img data-lazy={mimg2} alt='mimg2' />
+            <img data-lazy={mimg3} alt='mimg3' />
+            <img data-lazy={mimg4} alt='mimg4' />
+            <img data-lazy={mimg5} alt='mimg5' />
+            <img data-lazy={mimg6} alt='mimg6' />
+            <img data-lazy={mimg7} alt='mimg7' />
+            <img data-lazy={mimg8} alt='mimg8' />
+            <img data-lazy={mimg9} alt='mimg9' />
+            <img data-lazy={mimg10} alt='mimg10' />
+            <img data-lazy={mimg11} alt='mimg11' />
+            <img data-lazy={mimg12} alt='mimg12' />
+            <img data-lazy={mimg13} alt='mimg13' />
+            <img data-lazy={mimg14} alt='mimg14' />
+            <img data-lazy={mimg15} alt='mimg15' />
+            <img data-lazy={mimg16} alt='mimg16' />
+            <img data-lazy={mimg17} alt='mimg17' />
+            <img data-lazy={mimg18} alt='mimg18' />
+          </div>
+          <div className='images'>
+            <a name="winter" /> {/* eslint-disable-line */}
+            <h2>Зима:</h2>
+            <img data-lazy={wimg1} alt='wimg1' />
+            <img data-lazy={wimg2} alt='wimg2' />
+            <img data-lazy={wimg3} alt='wimg3' />
+            <img data-lazy={wimg4} alt='wimg4' />
+            <img data-lazy={wimg5} alt='wimg5' />
+            <img data-lazy={wimg6} alt='wimg6' />
+            <img data-lazy={wimg7} alt='wimg7' />
+            <img data-lazy={wimg8} alt='wimg8' />
+          </div>
         </div>
       </div>
       <div className='inner-nav'>
@@ -194,6 +196,6 @@ export default (props) => {
         <a href="#moments">Моменти</a>
         <a href="#winter">Зима</a>
       </div>
-    </div>
+    </React.Fragment>
   );
 }
