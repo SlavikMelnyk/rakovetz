72428ac3b417eb3c85c02436f4941dedd139c054
Yaroslav Melnyk <yaroslav.melnyk@dataart.com>
2019-09-20 17:07:12 +0300

    add small photos

--- src/gallery/Gallery.js
+++ src/gallery/Gallery.js
@@ -1,74 +1,137 @@
 import React from 'react';
 import './gallery.scss';
 
-import wimg1 from './imgs/winter/img-1.webp';
-import wimg2 from './imgs/winter/img-2.webp';
-import wimg3 from './imgs/winter/img-3.webp';
-import wimg4 from './imgs/winter/img-4.webp';
-import wimg5 from './imgs/winter/img-5.webp';
-import wimg6 from './imgs/winter/img-6.webp';
-import wimg7 from './imgs/winter/img-7.webp';
-import wimg8 from './imgs/winter/img-8.webp';
+import wimg1 from './imgs/winter/img-1.jpg';
+import wimg2 from './imgs/winter/img-2.jpg';
+import wimg3 from './imgs/winter/img-3.jpg';
+import wimg4 from './imgs/winter/img-4.jpg';
+import wimg5 from './imgs/winter/img-5.jpg';
+import wimg6 from './imgs/winter/img-6.jpg';
+import wimg7 from './imgs/winter/img-7.jpg';
+import wimg8 from './imgs/winter/img-8.jpg';
+import wimg1s from './imgs/winter/img-1-s.jpg';
+import wimg2s from './imgs/winter/img-2-s.jpg';
+import wimg3s from './imgs/winter/img-3-s.jpg';
+import wimg4s from './imgs/winter/img-4-s.jpg';
+import wimg5s from './imgs/winter/img-5-s.jpg';
+import wimg6s from './imgs/winter/img-6-s.jpg';
+import wimg7s from './imgs/winter/img-7-s.jpg';
+import wimg8s from './imgs/winter/img-8-s.jpg';
 
-import limg1 from './imgs/lake/img-1.webp';
-import limg2 from './imgs/lake/img-2.webp';
-import limg3 from './imgs/lake/img-3.webp';
-import limg4 from './imgs/lake/img-4.webp';
-import limg5 from './imgs/lake/img-5.webp';
-import limg6 from './imgs/lake/img-6.webp';
+import limg1 from './imgs/lake/img-1.jpg';
+import limg2 from './imgs/lake/img-2.jpg';
+import limg3 from './imgs/lake/img-3.jpg';
+import limg4 from './imgs/lake/img-4.jpg';
+import limg5 from './imgs/lake/img-5.jpg';
+import limg6 from './imgs/lake/img-6.jpg';
+import limg1s from './imgs/lake/img-1-s.jpg';
+import limg2s from './imgs/lake/img-2-s.jpg';
+import limg3s from './imgs/lake/img-3-s.jpg';
+import limg4s from './imgs/lake/img-4-s.jpg';
+import limg5s from './imgs/lake/img-5-s.jpg';
+import limg6s from './imgs/lake/img-6-s.jpg';
 
-import simg1 from './imgs/sauna/img-1.webp';
-import simg2 from './imgs/sauna/img-2.webp';
-import simg3 from './imgs/sauna/img-3.webp';
-import simg4 from './imgs/sauna/img-4.webp';
-import simg5 from './imgs/sauna/img-5.webp';
+import simg1 from './imgs/sauna/img-1.jpg';
+import simg2 from './imgs/sauna/img-2.jpg';
+import simg3 from './imgs/sauna/img-3.jpg';
+import simg4 from './imgs/sauna/img-4.jpg';
+import simg5 from './imgs/sauna/img-5.jpg';
+import simg1s from './imgs/sauna/img-1-s.jpg';
+import simg2s from './imgs/sauna/img-2-s.jpg';
+import simg3s from './imgs/sauna/img-3-s.jpg';
+import simg4s from './imgs/sauna/img-4-s.jpg';
+import simg5s from './imgs/sauna/img-5-s.jpg';
 
-import lcimg1 from './imgs/location/img-1.webp';
-import lcimg2 from './imgs/location/img-2.webp';
-import lcimg3 from './imgs/location/img-3.webp';
-import lcimg4 from './imgs/location/img-4.webp';
-import lcimg5 from './imgs/location/img-5.webp';
-import lcimg6 from './imgs/location/img-6.webp';
-import lcimg9 from './imgs/location/img-9.webp';
-import lcimg10 from './imgs/location/img-10.webp';
+import lcimg1 from './imgs/location/img-1.jpg';
+import lcimg2 from './imgs/location/img-2.jpg';
+import lcimg3 from './imgs/location/img-3.jpg';
+import lcimg4 from './imgs/location/img-4.jpg';
+import lcimg5 from './imgs/location/img-5.jpg';
+import lcimg6 from './imgs/location/img-6.jpg';
+import lcimg9 from './imgs/location/img-9.jpg';
+import lcimg10 from './imgs/location/img-10.jpg';
+import lcimg1s from './imgs/location/img-1-s.jpg';
+import lcimg2s from './imgs/location/img-2-s.jpg';
+import lcimg3s from './imgs/location/img-3-s.jpg';
+import lcimg4s from './imgs/location/img-4-s.jpg';
+import lcimg5s from './imgs/location/img-5-s.jpg';
+import lcimg6s from './imgs/location/img-6-s.jpg';
+import lcimg9s from './imgs/location/img-9-s.jpg';
+import lcimg10s from './imgs/location/img-10-s.jpg';
 
-import mimg1 from './imgs/moments/img-1.webp';
-import mimg2 from './imgs/moments/img-2.webp';
-import mimg3 from './imgs/moments/img-3.webp';
-import mimg4 from './imgs/moments/img-4.webp';
-import mimg5 from './imgs/moments/img-5.webp';
-import mimg6 from './imgs/moments/img-6.webp';
-import mimg7 from './imgs/moments/img-7.webp';
-import mimg8 from './imgs/moments/img-8.webp';
-import mimg9 from './imgs/moments/img-9.webp';
-import mimg10 from './imgs/moments/img-10.webp';
-import mimg11 from './imgs/moments/img-11.webp';
-import mimg12 from './imgs/moments/img-12.webp';
-import mimg13 from './imgs/moments/img-13.webp';
-import mimg14 from './imgs/moments/img-14.webp';
-import mimg15 from './imgs/moments/img-15.webp';
-import mimg16 from './imgs/moments/img-16.webp';
-import mimg17 from './imgs/moments/img-17.webp';
-import mimg18 from './imgs/moments/img-18.webp';
+import mimg1 from './imgs/moments/img-1.jpg';
+import mimg2 from './imgs/moments/img-2.jpg';
+import mimg3 from './imgs/moments/img-3.jpg';
+import mimg4 from './imgs/moments/img-4.jpg';
+import mimg5 from './imgs/moments/img-5.jpg';
+import mimg6 from './imgs/moments/img-6.jpg';
+import mimg7 from './imgs/moments/img-7.jpg';
+import mimg8 from './imgs/moments/img-8.jpg';
+import mimg9 from './imgs/moments/img-9.jpg';
+import mimg10 from './imgs/moments/img-10.jpg';
+import mimg11 from './imgs/moments/img-11.jpg';
+import mimg12 from './imgs/moments/img-12.jpg';
+import mimg13 from './imgs/moments/img-13.jpg';
+import mimg14 from './imgs/moments/img-14.jpg';
+import mimg15 from './imgs/moments/img-15.jpg';
+import mimg16 from './imgs/moments/img-16.jpg';
+import mimg17 from './imgs/moments/img-17.jpg';
+import mimg18 from './imgs/moments/img-18.jpg';
+import mimg1s from './imgs/moments/img-1-s.jpg';
+import mimg2s from './imgs/moments/img-2-s.jpg';
+import mimg3s from './imgs/moments/img-3-s.jpg';
+import mimg4s from './imgs/moments/img-4-s.jpg';
+import mimg5s from './imgs/moments/img-5-s.jpg';
+import mimg6s from './imgs/moments/img-6-s.jpg';
+import mimg7s from './imgs/moments/img-7-s.jpg';
+import mimg8s from './imgs/moments/img-8-s.jpg';
+import mimg9s from './imgs/moments/img-9-s.jpg';
+import mimg10s from './imgs/moments/img-10-s.jpg';
+import mimg11s from './imgs/moments/img-11-s.jpg';
+import mimg12s from './imgs/moments/img-12-s.jpg';
+import mimg13s from './imgs/moments/img-13-s.jpg';
+import mimg14s from './imgs/moments/img-14-s.jpg';
+import mimg15s from './imgs/moments/img-15-s.jpg';
+import mimg16s from './imgs/moments/img-16-s.jpg';
+import mimg17s from './imgs/moments/img-17-s.jpg';
+import mimg18s from './imgs/moments/img-18-s.jpg';
 
-import himg1 from './imgs/houses/img-1.webp';
-import himg2 from './imgs/houses/img-2.webp';
-import himg3 from './imgs/houses/img-3.webp';
-import himg4 from './imgs/houses/img-4.webp';
-import himg5 from './imgs/houses/img-5.webp';
-import himg6 from './imgs/houses/img-6.webp';
-import himg7 from './imgs/houses/img-7.webp';
-import himg8 from './imgs/houses/img-8.webp';
-import himg9 from './imgs/houses/img-9.webp';
-import himg10 from './imgs/houses/img-10.webp';
-import himg11 from './imgs/houses/img-11.webp';
-import himg12 from './imgs/houses/img-12.webp';
-import himg13 from './imgs/houses/img-13.webp';
-import himg14 from './imgs/houses/img-14.webp';
-import himg15 from './imgs/houses/img-15.webp';
-import himg16 from './imgs/houses/img-16.webp';
-import himg17 from './imgs/houses/img-17.webp';
-import himg18 from './imgs/houses/img-18.webp';
+import himg1 from './imgs/houses/img-1.jpg';
+import himg2 from './imgs/houses/img-2.jpg';
+import himg3 from './imgs/houses/img-3.jpg';
+import himg4 from './imgs/houses/img-4.jpg';
+import himg5 from './imgs/houses/img-5.jpg';
+import himg6 from './imgs/houses/img-6.jpg';
+import himg7 from './imgs/houses/img-7.jpg';
+import himg8 from './imgs/houses/img-8.jpg';
+import himg9 from './imgs/houses/img-9.jpg';
+import himg10 from './imgs/houses/img-10.jpg';
+import himg11 from './imgs/houses/img-11.jpg';
+import himg12 from './imgs/houses/img-12.jpg';
+import himg13 from './imgs/houses/img-13.jpg';
+import himg14 from './imgs/houses/img-14.jpg';
+import himg15 from './imgs/houses/img-15.jpg';
+import himg16 from './imgs/houses/img-16.jpg';
+import himg17 from './imgs/houses/img-17.jpg';
+import himg18 from './imgs/houses/img-18.jpg';
+import himg1s from './imgs/houses/img-1-s.jpg';
+import himg2s from './imgs/houses/img-2-s.jpg';
+import himg3s from './imgs/houses/img-3-s.jpg';
+import himg4s from './imgs/houses/img-4-s.jpg';
+import himg5s from './imgs/houses/img-5-s.jpg';
+import himg6s from './imgs/houses/img-6-s.jpg';
+import himg7s from './imgs/houses/img-7-s.jpg';
+import himg8s from './imgs/houses/img-8-s.jpg';
+import himg9s from './imgs/houses/img-9-s.jpg';
+import himg10s from './imgs/houses/img-10-s.jpg';
+import himg11s from './imgs/houses/img-11-s.jpg';
+import himg12s from './imgs/houses/img-12-s.jpg';
+import himg13s from './imgs/houses/img-13-s.jpg';
+import himg14s from './imgs/houses/img-14-s.jpg';
+import himg15s from './imgs/houses/img-15-s.jpg';
+import himg16s from './imgs/houses/img-16-s.jpg';
+import himg17s from './imgs/houses/img-17-s.jpg';
+import himg18s from './imgs/houses/img-18-s.jpg';
 
 export default (props) => {
   
@@ -80,7 +143,7 @@ export default (props) => {
         entries.forEach(entry => {
           if(entry.isIntersecting) {
             const img = entry.target;
-            const src = window.innerHeight <= 768 ? img.getAttribute('data-lazy-min') : img.getAttribute('data-lazy');
+            const src = window.innerWidth <= 815 ? img.getAttribute('data-lazy-min') : img.getAttribute('data-lazy');
             img.setAttribute('src', src);
             img.classList.add('fade');
             
@@ -102,89 +165,89 @@ export default (props) => {
           <div className='images'>
             <a name="houses" /> {/* eslint-disable-line */}
             <h2>Котеджі:</h2>
-            <img data-lazy={himg1} alt='himg1' />
-            <img data-lazy={himg2} alt='himg2' />
-            <img data-lazy={himg3} alt='himg3' />
-            <img data-lazy={himg4} alt='himg4' />
-            <img data-lazy={himg5} alt='himg5' />
-            <img data-lazy={himg6} alt='himg6' />
-            <img data-lazy={himg7} alt='himg7' />
-            <img data-lazy={himg8} alt='himg8' />
-            <img data-lazy={himg9} alt='himg9' />
-            <img data-lazy={himg10} alt='himg10' />
-            <img data-lazy={himg11} alt='himg11' />
-            <img data-lazy={himg12} alt='himg12' />
-            <img data-lazy={himg13} alt='himg13' />
-            <img data-lazy={himg14} alt='himg14' />
-            <img data-lazy={himg15} alt='himg15' />
-            <img data-lazy={himg16} alt='himg16' />
-            <img data-lazy={himg17} alt='himg17' />
-            <img data-lazy={himg18} alt='himg18' />
+            <img data-lazy-min={himg1s} data-lazy={himg1} alt='himg1' />
+            <img data-lazy-min={himg2s} data-lazy={himg2} alt='himg2' />
+            <img data-lazy-min={himg3s} data-lazy={himg3} alt='himg3' />
+            <img data-lazy-min={himg4s} data-lazy={himg4} alt='himg4' />
+            <img data-lazy-min={himg5s} data-lazy={himg5} alt='himg5' />
+            <img data-lazy-min={himg6s} data-lazy={himg6} alt='himg6' />
+            <img data-lazy-min={himg7s} data-lazy={himg7} alt='himg7' />
+            <img data-lazy-min={himg8s} data-lazy={himg8} alt='himg8' />
+            <img data-lazy-min={himg9s} data-lazy={himg9} alt='himg9' />
+            <img data-lazy-min={himg10s} data-lazy={himg10} alt='himg10' />
+            <img data-lazy-min={himg11s} data-lazy={himg11} alt='himg11' />
+            <img data-lazy-min={himg12s} data-lazy={himg12} alt='himg12' />
+            <img data-lazy-min={himg13s} data-lazy={himg13} alt='himg13' />
+            <img data-lazy-min={himg14s} data-lazy={himg14} alt='himg14' />
+            <img data-lazy-min={himg15s} data-lazy={himg15} alt='himg15' />
+            <img data-lazy-min={himg16s} data-lazy={himg16} alt='himg16' />
+            <img data-lazy-min={himg17s} data-lazy={himg17} alt='himg17' />
+            <img data-lazy-min={himg18s} data-lazy={himg18} alt='himg18' />
           </div>
           <div className='images'>
             <a name="sauna" /> {/* eslint-disable-line */}
             <h2>Сауна/Чан:</h2>
-            <img data-lazy={simg1} alt='simg1' />
-            <img data-lazy={simg2} alt='simg2' />
-            <img data-lazy={simg3} alt='simg3' />
-            <img data-lazy={simg4} alt='simg4' />
-            <img data-lazy={simg5} alt='simg5' />
+            <img data-lazy-min={simg1s} data-lazy={simg1} alt='simg1' />
+            <img data-lazy-min={simg2s} data-lazy={simg2} alt='simg2' />
+            <img data-lazy-min={simg3s} data-lazy={simg3} alt='simg3' />
+            <img data-lazy-min={simg4s} data-lazy={simg4} alt='simg4' />
+            <img data-lazy-min={simg5s} data-lazy={simg5} alt='simg5' />
           </div>
           <div className='images'>
             <a name="lake" /> {/* eslint-disable-line */}
             <h2>Альтанка на озері:</h2>
-            <img data-lazy={limg1} alt='limg1' />
-            <img data-lazy={limg2} alt='limg2' />
-            <img data-lazy={limg3} alt='limg3' />
-            <img data-lazy={limg4} alt='limg4' />
-            <img data-lazy={limg5} alt='limg5' />
-            <img data-lazy={limg6} alt='limg6' />
+            <img data-lazy-min={limg1s} data-lazy={limg1} alt='limg1' />
+            <img data-lazy-min={limg2s} data-lazy={limg2} alt='limg2' />
+            <img data-lazy-min={limg3s} data-lazy={limg3} alt='limg3' />
+            <img data-lazy-min={limg4s} data-lazy={limg4} alt='limg4' />
+            <img data-lazy-min={limg5s} data-lazy={limg5} alt='limg5' />
+            <img data-lazy-min={limg6s} data-lazy={limg6} alt='limg6' />
           </div>
           <div className='images'>
             <a name="location" /> {/* eslint-disable-line */}
             <h2>Локація:</h2>
-            <img data-lazy={lcimg1} alt='lcimg1' />
-            <img data-lazy={lcimg2} alt='lcimg2' />
-            <img data-lazy={lcimg3} alt='lcimg3' />
-            <img data-lazy={lcimg4} alt='lcimg4' />
-            <img data-lazy={lcimg5} alt='lcimg5' />
-            <img data-lazy={lcimg6} alt='lcimg6' />
-            <img data-lazy={lcimg9} alt='lcimg9' />
-            <img data-lazy={lcimg10} alt='lcimg10' />
+            <img data-lazy-min={lcimg1s} data-lazy={lcimg1} alt='lcimg1' />
+            <img data-lazy-min={lcimg2s} data-lazy={lcimg2} alt='lcimg2' />
+            <img data-lazy-min={lcimg3s} data-lazy={lcimg3} alt='lcimg3' />
+            <img data-lazy-min={lcimg4s} data-lazy={lcimg4} alt='lcimg4' />
+            <img data-lazy-min={lcimg5s} data-lazy={lcimg5} alt='lcimg5' />
+            <img data-lazy-min={lcimg6s} data-lazy={lcimg6} alt='lcimg6' />
+            <img data-lazy-min={lcimg9s} data-lazy={lcimg9} alt='lcimg9' />
+            <img data-lazy-min={lcimg10s} data-lazy={lcimg10} alt='lcimg10' />
           </div>
           <div className='images'>
             <a name="moments" /> {/* eslint-disable-line */}
             <h2>Моменти:</h2>
-            <img data-lazy={mimg1} alt='mimg1' />
-            <img data-lazy={mimg2} alt='mimg2' />
-            <img data-lazy={mimg3} alt='mimg3' />
-            <img data-lazy={mimg4} alt='mimg4' />
-            <img data-lazy={mimg5} alt='mimg5' />
-            <img data-lazy={mimg6} alt='mimg6' />
-            <img data-lazy={mimg7} alt='mimg7' />
-            <img data-lazy={mimg8} alt='mimg8' />
-            <img data-lazy={mimg9} alt='mimg9' />
-            <img data-lazy={mimg10} alt='mimg10' />
-            <img data-lazy={mimg11} alt='mimg11' />
-            <img data-lazy={mimg12} alt='mimg12' />
-            <img data-lazy={mimg13} alt='mimg13' />
-            <img data-lazy={mimg14} alt='mimg14' />
-            <img data-lazy={mimg15} alt='mimg15' />
-            <img data-lazy={mimg16} alt='mimg16' />
-            <img data-lazy={mimg17} alt='mimg17' />
-            <img data-lazy={mimg18} alt='mimg18' />
+            <img data-lazy-min={mimg1s} data-lazy={mimg1} alt='mimg1' />
+            <img data-lazy-min={mimg2s} data-lazy={mimg2} alt='mimg2' />
+            <img data-lazy-min={mimg3s} data-lazy={mimg3} alt='mimg3' />
+            <img data-lazy-min={mimg4s} data-lazy={mimg4} alt='mimg4' />
+            <img data-lazy-min={mimg5s} data-lazy={mimg5} alt='mimg5' />
+            <img data-lazy-min={mimg6s} data-lazy={mimg6} alt='mimg6' />
+            <img data-lazy-min={mimg7s} data-lazy={mimg7} alt='mimg7' />
+            <img data-lazy-min={mimg8s} data-lazy={mimg8} alt='mimg8' />
+            <img data-lazy-min={mimg9s} data-lazy={mimg9} alt='mimg9' />
+            <img data-lazy-min={mimg10s} data-lazy={mimg10} alt='mimg10' />
+            <img data-lazy-min={mimg11s} data-lazy={mimg11} alt='mimg11' />
+            <img data-lazy-min={mimg12s} data-lazy={mimg12} alt='mimg12' />
+            <img data-lazy-min={mimg13s} data-lazy={mimg13} alt='mimg13' />
+            <img data-lazy-min={mimg14s} data-lazy={mimg14} alt='mimg14' />
+            <img data-lazy-min={mimg15s} data-lazy={mimg15} alt='mimg15' />
+            <img data-lazy-min={mimg16s} data-lazy={mimg16} alt='mimg16' />
+            <img data-lazy-min={mimg17s} data-lazy={mimg17} alt='mimg17' />
+            <img data-lazy-min={mimg18s} data-lazy={mimg18} alt='mimg18' />
           </div>
           <div className='images'>
             <a name="winter" /> {/* eslint-disable-line */}
             <h2>Зима:</h2>
-            <img data-lazy={wimg1} alt='wimg1' />
-            <img data-lazy={wimg2} alt='wimg2' />
-            <img data-lazy={wimg3} alt='wimg3' />
-            <img data-lazy={wimg4} alt='wimg4' />
-            <img data-lazy={wimg5} alt='wimg5' />
-            <img data-lazy={wimg6} alt='wimg6' />
-            <img data-lazy={wimg7} alt='wimg7' />
-            <img data-lazy={wimg8} alt='wimg8' />
+            <img data-lazy-min={wimg1s} data-lazy={wimg1} alt='wimg1' />
+            <img data-lazy-min={wimg2s} data-lazy={wimg2} alt='wimg2' />
+            <img data-lazy-min={wimg3s} data-lazy={wimg3} alt='wimg3' />
+            <img data-lazy-min={wimg4s} data-lazy={wimg4} alt='wimg4' />
+            <img data-lazy-min={wimg5s} data-lazy={wimg5} alt='wimg5' />
+            <img data-lazy-min={wimg6s} data-lazy={wimg6} alt='wimg6' />
+            <img data-lazy-min={wimg7s} data-lazy={wimg7} alt='wimg7' />
+            <img data-lazy-min={wimg8s} data-lazy={wimg8} alt='wimg8' />
           </div>
         </div>
       </div>
